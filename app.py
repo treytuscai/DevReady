d75c95e6d7e78c1f09432b04965cd54b77608107
Ryan Mogauro <89362786+ryanmogauro@users.noreply.github.com>
2025-05-07 19:15:47 -0400

    added compression

--- app.py
+++ app.py
@@ -1,7 +1,8 @@
 """This module initializes DevReady."""
 from website import create_app
+from flask_compress import Compress
 
 app = create_app()
-
+Compress(app)
 if __name__ == '__main__':
     app.run(debug=True)
